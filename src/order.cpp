3740cb653684d2bd06d320589bef8872a471029f
Rohan Shah <rohan.shah@bookshah.com>
2016-01-01 01:12:17 +1100

    Fixed typo

--- src/order.cpp
+++ src/order.cpp
@@ -21,7 +21,7 @@ namespace mpMapInteractive
 			for(int j = 0; j < nSubMarkers; j++)
 			{
 				double* dest = mem + i + j * nSubMarkers;
-				int row = permutation[i + startIndex], column = permutation[i + startIndex];
+				int row = permutation[i + startIndex], column = permutation[j + startIndex];
 				if(row > column) std::swap(row, column);
 				*dest = levels[originalRawData[(column * (column + 1))/2 + row]];
 				nonZero |= (*dest != 0);
